c7714f3d4c5d233d4d3f0581f1431ee052ddd61a
SiloSix <SiloSix@M6500A>
2018-06-14 09:26:34 -0500

    Polling Triggered2

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	  public static void main(String[] args){
 		    
-		    System.out.println("Polling Triggered");
+		    System.out.println("Polling Triggered2");
 		    for (int i = 0; i < 10; i++) {
 		    	System.out.println("Iteration: " + i);
 			}
